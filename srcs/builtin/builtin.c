cf079eaa5c67deece9fc6275a56eebda17e8d50d
moi <alexdiehlboitel@gmail.com>
2025-01-13 04:45:50 +0100

    export can workk i think ? please ?

--- srcs/builtin/builtin.c
+++ srcs/builtin/builtin.c
@@ -1,33 +1,57 @@
 #include "minishell.h"
 
 //check if no options before 
-int ft_pwd(void)
-{
-    char *cwd;
+// int ft_pwd(void)
+// {
+//     char *cwd;
 
-    cwd = getcwd(NULL, 0);
-    if (cwd = NULL)
+//     cwd = getcwd(NULL, 0);
+//     if (cwd = NULL)
+//     {
+//         perror("getcwd");
+//         return(EXIT_FAILURE);
+//     }
+//     printf("%s\n", cwd);
+//     free(cwd);
+//     return(EXIT_SUCCESS);
+// }
+
+// //check if no arguments or options before 
+// int ft_envp(char **envp)
+// {
+//     while(*envp)
+//     {
+//         printf("%s\n", *envp);
+//         envp++;
+//     }
+//     return(EXIT_SUCCESS);
+// }
+
+
+int ft_echo(t_token *tokens)
+{
+    int newline;
+    t_token *token;
+    int first;
+
+    first = 1;
+    newline = 1;
+    token = token->next; 
+    if (token && strcmp(token->str, "-n") == 0)
     {
-        perror("getcwd");
-        return(EXIT_FAILURE);
+        newline = 0;
+        token = token->next;
     }
-    printf("%s\n", cwd);
-    free(cwd);
-    return(EXIT_SUCCESS);
-}
-
-//check if no arguments or options before 
-int ft_envp(char **envp)
-{
-    while(*envp)
+    while (token)
     {
-        printf("%s\n", *envp);
-        envp++;
+        if (!first)
+            printf(" ");
+        printf("%s", token->str);
+        first = 0;
+        token = token->next;
+
     }
-    return(EXIT_SUCCESS);
-}
-
-int ft_echo(t_token )
-{
-
+    if (newline)
+        printf("\n");
+    return (0);
 }
---
+++ srcs/builtin/export_builtin.c
@@ -0,0 +1,95 @@
+#include "minishell.h"
+
+int is_valid_identifier(char *str)
+{
+    int i;
+
+    if (!str || !*str || ft_isdigit(*str))
+        return (EXIT_SUCCESS);
+    i = 0;
+    while (str[i])
+    {
+        if (!ft_isalnum(str[i]) && str[i] != '_')
+            return (0);
+        i++;
+    }
+    return (EXIT_FAILURE);
+}
+
+char *get_var_name(char *str)
+{
+    int i;
+    char *name;
+
+    i = 0;
+    while (str[i] && str[i] != '=')
+        i++;
+    name = ft_strndup(str, i);
+    return (name);
+}
+
+void update_env_var(char ***env, char *var)
+{
+    int i;
+    char *name;
+    char **new_env;
+
+    name = get_var_name(var);
+    i = 0;
+    while ((*env)[i])
+    {
+        if (ft_strncmp((*env)[i], name, ft_strlen(name)) == 0 
+            && ((*env)[i][ft_strlen(name)] == '=' 
+            || (*env)[i][ft_strlen(name)] == '\0'))
+        {
+            free((*env)[i]);
+            (*env)[i] = ft_strdup(var);
+            free(name);
+            return;
+        }
+        i++;
+    }
+    new_env = malloc(sizeof(char *) * (i + 2));
+    if (!new_env)
+    {
+        free(name);
+        return;
+    }
+    i = 0;
+    while ((*env)[i])
+    {
+        new_env[i] = ft_strdup((*env)[i]);
+        i++;
+    }
+    new_env[i] = ft_strdup(var);
+    new_env[i + 1] = NULL;
+    ft_free_array(*env);
+    *env = new_env;
+    free(name);
+}
+
+int ft_export(char **args, char ***env)
+{
+    int i;
+    char *name;
+
+    if (!args[1])
+        return (0);
+    i = 1;
+    while (args[i])
+    {
+        if (ft_strchr(args[i], '='))
+        {
+            name = get_var_name(args[i]);
+            if (is_valid_identifier(name))
+                update_env_var(env, args[i]);
+            else
+            {
+                printf("no write better %s\n", args[i]);
+            }
+            free(name);
+        }
+        i++;
+    }
+    return (EXIT_SUCCESS);
+}
