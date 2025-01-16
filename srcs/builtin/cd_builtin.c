bd7873d549b270dc0113757e89a9de3426c909ed
moi <alexdiehlboitel@gmail.com>
2025-01-16 18:00:49 +0100

    unset builtin & some builtin correcter still some changes to do

--- srcs/builtin/cd_builtin.c
+++ srcs/builtin/cd_builtin.c
@@ -14,7 +14,52 @@ char *get_home_dir(char **envp)
     return (NULL);
 }
 
-void    update_pwd_vars(char **envp)
+int ft_setenv(char **envp, const char *name, const char *value)
+{
+    int     i;
+    int     len;
+    int     name_len;
+    int     value_len;
+    char    *new_var;
+
+    name_len = strlen(name);
+    value_len = strlen(value);
+    len = name_len + value_len + 2;
+    new_var = malloc(len);
+    if (!new_var)
+        return (-1);
+    i = 0;
+    while (name[i])
+    {
+        new_var[i] = name[i];
+        i++;
+    }
+    new_var[i] = '=';
+    i++;
+    len = 0;
+    while (value[len])
+    {
+        new_var[i + len] = value[len];
+        len++;
+    }
+    new_var[i + len] = '\0';
+    i = 0;
+    while (envp[i])
+    {
+        if (strncmp(envp[i], name, name_len) == 0 && envp[i][name_len] == '=')
+        {
+            free(envp[i]);
+            envp[i] = new_var;
+            return (0);
+        }
+        i++;
+    }
+    envp[i] = new_var;
+    envp[i + 1] = NULL;
+    return (0);
+}
+
+void    update_pwd_vars(char **env)
 {
     char    *oldpwd;
     char    *pwd;
@@ -22,34 +67,36 @@ void    update_pwd_vars(char **envp)
 
     oldpwd = getenv("PWD");
     if (oldpwd)
-        setenv("OLDPWD", oldpwd, 1);
+        ft_setenv(env, "OLDPWD", oldpwd);
     if (getcwd(current_dir, sizeof(current_dir)))
-        setenv("PWD", current_dir, 1);
+        ft_setenv(env, "PWD", current_dir);
 }
 
-int cd_builtin(char **args, char **envp)
+int cd_builtin(t_token *token, char **env)
 {
     char    *path;
+    t_token *arg;
     
-    if (!args[1])
+    arg = token->next;
+    if (!arg)
     {
-        path = get_home_dir(envp);
+        path = get_home_dir(env);
         if (!path)
         {
             printf("cd: HOME not set\n");
             return (EXIT_FAILURE);
         }
     }
-    else if (strcmp(args[1], "~") == 0)
+    else if (strcmp(arg->str, "~") == 0)
     {
-        path = get_home_dir(envp);
+        path = get_home_dir(env);
         if (!path)
         {
             printf("cd: HOME not set\n");
             return (EXIT_FAILURE);
         }
     }
-    else if (strcmp(args[1], "-") == 0)
+    else if (strcmp(arg->str, "-") == 0)
     {
         path = getenv("OLDPWD");
         if (!path)
@@ -60,12 +107,12 @@ int cd_builtin(char **args, char **envp)
         printf("%s\n", path);
     }
     else
-        path = args[1];
+        path = arg->str;
     if (chdir(path) != 0)
     {
         perror("cd");
         return (EXIT_FAILURE);
     }
-    update_pwd_vars(envp);
+    update_pwd_vars(env);
     return (EXIT_SUCCESS);
 }
